966c16995cb93f2d38c31956495b31d25c62b230
Guillaume HARARI <guillaume.harari@wildcodeschool.fr>
2020-04-23 17:10:33 +0200

    API KEY in global constant gitignored

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 if (getenv('ENV') === false) {
     require_once __DIR__ . '/../config/debug.php';
     require_once __DIR__ . '/../config/db.php';
+    require_once __DIR__ . '/../config/api.php';
 }
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../src/routing.php';
--- src/Controller/NasaController.php
+++ src/Controller/NasaController.php
@@ -7,7 +7,7 @@ class NasaController extends AbstractController
 {
     public function picture()
     {
-        $picData = $this->get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
+        $picData = $this->get('https://api.nasa.gov/planetary/apod?api_key=' . APP_API_KEY);
 
         return $this->twig->render('Nasa/picture.html.twig', [
             'pic_data' => $picData,
